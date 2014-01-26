3aabeb11a02b21f277d5ea4241a88749b9efe386
Simon Jefford <si@ibrokethat.com>
2014-01-26 21:56:17 +0000

    updated paths to use process.cwd to allow use of module as a dependency

--- server.js
+++ server.js
@@ -1,47 +1,83 @@
 // Define third-party libraries
-var util    = require('util');
+var util = require('util');
 var cluster = require('cluster');
 var express = require('express');
-var app     = express();
-var CONF    = require('config');
+var app = express();
+var CONF = require('config');
+var forEach = require('super-iter').forEach;
 // var log     = require('metalogger')();
 
-if ('log' in CONF) {
 
-  if ('plugin' in CONF.log) { process.env.NODE_LOGGER_PLUGIN = CONF.log.plugin; }
-  if ('level'  in CONF.log) { process.env.NODE_LOGGER_LEVEL  = CONF.log.level; }
+//  do this properly
+function updateConf (defaultConf, hostConf) {
 
-  if ('customlevels' in CONF.log) {
-    for (var key in CONF.log.customlevels) {
-      process.env['NODE_LOGGER_LEVEL_' + key] = CONF.log.customlevels[key];
+  forEach(hostConf, function (value, key) {
+    defaultConf[key] = value;
+  });
+
+}
+
+function setLogLevels () {
+
+  if ('log' in CONF) {
+
+    if ('plugin' in CONF.log) { process.env.NODE_LOGGER_PLUGIN = CONF.log.plugin; }
+    if ('level'  in CONF.log) { process.env.NODE_LOGGER_LEVEL  = CONF.log.level; }
+
+    if ('customlevels' in CONF.log) {
+      for (var key in CONF.log.customlevels) {
+        process.env['NODE_LOGGER_LEVEL_' + key] = CONF.log.customlevels[key];
+      }
     }
+  }
+
+}
+
+function start () {
+
+  // require('./lib/services').setup();
+  require('./lib/app/server').setup(app);
+
+  var isClusterMaster = (cluster.isMaster && (process.env.NODE_CLUSTERED == 1));
+
+  var is_http_thread = true;
+  if (isClusterMaster ||
+      ( 'undefined' !== typeof process.env.NODE_ISNOT_HTTP_SERVER_THREAD &&
+          process.env.NODE_ISNOT_HTTP_SERVER_THREAD != 'true')) {
+    is_http_thread = false;
+  }
+
+
+  console.log("is http thread? " + is_http_thread);
+
+  if (isClusterMaster) {
+    require('./lib/clustering').setup();
+  }
+
+  if (is_http_thread) {
+    app.listen(CONF.app.port);
+  }
+
+  // If we are not running a cluster at all:
+  if (!isClusterMaster && cluster.isMaster) {
+    console.log("Express server instance listening on port " + CONF.app.port);
+  }
+
+
+}
+
+
+
+module.exports = function superposition (conf) {
+
+  updateConf(CONF, conf);
+  setLogLevels();
+
+  return {
+    start: start
   }
 }
 
-// require('./lib/services').setup();
-require('./lib/app/server').setup(app);
 
-var isClusterMaster = (cluster.isMaster && (process.env.NODE_CLUSTERED == 1));
-
-var is_http_thread = true;
-if (isClusterMaster ||
-    ( 'undefined' !== typeof process.env.NODE_ISNOT_HTTP_SERVER_THREAD &&
-        process.env.NODE_ISNOT_HTTP_SERVER_THREAD != 'true')) {
-  is_http_thread = false;
-}
-
-
-console.log("is http thread? " + is_http_thread);
-
-if (isClusterMaster) {
-  require('./lib/clustering').setup();
-}
-
-if (is_http_thread) {
-  app.listen(CONF.app.port);
-}
-
-// If we are not running a cluster at all:
-if (!isClusterMaster && cluster.isMaster) {
-  console.log("Express server instance listening on port " + CONF.app.port);
-}
+module.exports.updateConf = updateConf;
+module.exports.setLogLevels = setLogLevels;
